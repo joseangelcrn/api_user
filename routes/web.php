bed97704bb3cfe75a194b0936ae296c0f022c44a
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-21 15:45:52 +0200

    default api routes added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signUp');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+
+    });
 });
