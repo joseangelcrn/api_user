51098e883002e9c457fec171112c3baa8ca968af
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-21 19:44:43 +0200

    Routes moved to routes/api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class,'login']);
+Route::post('signup', [AuthController::class,'signUp']);
+
+Route::get('logout', [AuthController::class,'logout']);
+Route::get('user', [AuthController::class,'user']);
