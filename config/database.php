82fda01a5c45715d0ec8a84e8ec1f4ff196d0c79
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-22 21:13:08 +0200

    change collation

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
