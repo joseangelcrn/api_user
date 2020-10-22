eaeba04a50192bee256390dc6338f7dd2622d9a9
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-22 19:25:03 +0200

    Root route added as info page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function (Request $request) {
+    return view('welcome');
+});
