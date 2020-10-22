08578863cd34ea3f60b80d161022c86e1536aa0a
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-10-22 12:20:54 +0200

    delete unused route

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::post('login', [AuthController::class,'login']);
 Route::post('signup', [AuthController::class,'signUp']);
